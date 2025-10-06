5b2cf6afc6eed922c9a2378a2ca5b6b6d70128e8
Lorenzo Colitti <lorenzo@google.com>
2025-10-05 20:42:25 -0700

    Don't probe IPv6 Public DNS if there is no default route.
    There's no point attempting to probe global IPv6 addresses unless
    the device has both a global IPv6 address and a default route.
    
    Note that this does change the behaviour change: these DNS servers
    will not be probed if the device only has deprecated IPv6 addresses
    (because isGlobalPreferred returns false for a deprecated address).
    That seems acceptable given that IpClient won't include DNS servers
    in LinkProperties unless LinkProperties#isReachable returns true,
    which it won't if there is no global preferred address.
    
    This fixes NetworkDiagnostics logging errors like:
    
    10-06 08:47:07.098  1000   592   987 E NetworkDiagnostics: Can't get MTU for destination /2001:4860:4860::8888
    10-06 08:47:07.098  1000   592   987 E NetworkDiagnostics: android.system.ErrnoException: connect failed: ENETUNREACH (Network is unreachable)
    10-06 08:47:07.098  1000   592   987 E NetworkDiagnostics: 	at libcore.io.Linux.connect(Native Method)
    10-06 08:47:07.098  1000   592   987 E NetworkDiagnostics: 	at libcore.io.ForwardingOs.connect(ForwardingOs.java:201)
    10-06 08:47:07.098  1000   592   987 E NetworkDiagnostics: 	at libcore.io.BlockGuardOs.connect(BlockGuardOs.java:158)
    10-06 08:47:07.098  1000   592   987 E NetworkDiagnostics: 	at android.system.Os.connect(Os.java:134)
    10-06 08:47:07.098  1000   592   987 E NetworkDiagnostics: 	at android.net.connectivity.com.android.server.connectivity.NetworkDiagnostics.getMtuForTarget(NetworkDiagnostics.java:347)
    10-06 08:47:07.098  1000   592   987 E NetworkDiagnostics: 	at android.net.connectivity.com.android.server.connectivity.NetworkDiagnostics.prepareIcmpMeasurements(NetworkDiagnostics.java:297)
    
    Test: None
    Change-Id: Icc705c16f81f77ebc11a5df03e0f7c8ca462494d

--- service/src/com/android/server/connectivity/NetworkDiagnostics.java
+++ service/src/com/android/server/connectivity/NetworkDiagnostics.java
@@ -213,7 +213,7 @@ public class NetworkDiagnostics {
         // TODO: we could use mLinkProperties.isReachable(TEST_DNS6) here, because we won't set any
         // DNS servers for which isReachable() is false, but since this is diagnostic code, be extra
         // careful.
-        if (mLinkProperties.hasGlobalIpv6Address() || mLinkProperties.hasIpv6DefaultRoute()) {
+        if (mLinkProperties.hasGlobalIpv6Address() && mLinkProperties.hasIpv6DefaultRoute()) {
             mLinkProperties.addDnsServer(TEST_DNS6);
         }
 
