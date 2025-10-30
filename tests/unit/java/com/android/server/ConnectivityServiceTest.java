2928474548615314e2025283081550f39e98de4e
Remi NGUYEN VAN <reminv@google.com>
2025-10-30 13:08:17 +0900

    Enforce ACCESS_LOCAL_NETWORK
    Instead of checking for NEARBY_WIFI_DEVICES when apps have opted in to
    RESTRICT_LOCAL_NETWORK, use ACCESS_LOCAL_NETWORK if the flag is enabled.
    
    This only affects apps targeting SDK 37, since ACCESS_LOCAL_NETWORK is a
    split-permission from INTERNET with targetSdk=37.
    
    Bug: 428742474
    Test: atest
    Flag: android.permission.flags.access_local_network_permission_enabled
    Change-Id: I55b81e0d6430ff1db6131d1743c1326aebd437b8

--- tests/unit/java/com/android/server/ConnectivityServiceTest.java
+++ tests/unit/java/com/android/server/ConnectivityServiceTest.java
@@ -408,6 +408,7 @@ import com.android.server.ConnectivityService.ConnectivityDiagnosticsCallbackInf
 import com.android.server.ConnectivityService.NetworkRequestInfo;
 import com.android.server.ConnectivityServiceTest.ConnectivityServiceDependencies.DestroySocketsWrapper;
 import com.android.server.ConnectivityServiceTest.ConnectivityServiceDependencies.ReportedInterfaces;
+import com.android.server.connectivity.AppOptInDefaultNetworkController;
 import com.android.server.connectivity.AppOptInDefaultNetworkPolicy;
 import com.android.server.connectivity.ApplicationSelfCertifiedNetworkCapabilities;
 import com.android.server.connectivity.AutomaticOnOffKeepaliveTracker;
@@ -427,7 +428,6 @@ import com.android.server.connectivity.PermissionMonitor;
 import com.android.server.connectivity.ProxyTracker;
 import com.android.server.connectivity.QosCallbackTracker;
 import com.android.server.connectivity.QuicConnectionCloser;
-import com.android.server.connectivity.AppOptInDefaultNetworkController;
 import com.android.server.connectivity.TcpKeepaliveController;
 import com.android.server.connectivity.UidRangeUtils;
 import com.android.server.net.NetworkPinner;
@@ -2470,7 +2470,7 @@ public class ConnectivityServiceTest {
 
     static class PermissionMonitorDependencies extends PermissionMonitor.Dependencies {
         @Override
-        public boolean shouldEnforceLocalNetRestrictions(int uid) {
+        public boolean isOptedInToLocalNetworkRestrictions(int uid) {
             return false;
         }
 
