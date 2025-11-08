281a8d278524dea79c64ecaf1e22d65781e71933
Chris Desir <cdesir@google.com>
2025-11-07 16:13:54 -0800

    Change ETH_REQUEST to filter out physical interfaces
    On automotive devices, physical interfaces that were satisfying
    ETH_REQUEST were being interpreted in the tests in listeners and
    callbacks, leading to many test failures.
    
    This change alters ETH_REQUEST to be more selective towards the test
    interfaces created. Transports currently match as an OR, as opposed to
    how capabilities are treated as an AND. Therefore, the previous request
    could match either Ethernet or Test transports, or networks that had
    both Ethernet or Test transports. This is undesirable, and now, only
    test transports will be matched. Additionally, NOT_METERED is one of the
    few capabilities contained in test interfaces but not physical.
    
    Test: atest EthernetManagerTest with eth0 configured
    Bug: 448846480
    Flag: EXEMPT bugfix
    Change-Id: Idfef6bf0ce5841ca4dc7c4d8e956dce846afab3d

--- tests/cts/net/src/android/net/cts/EthernetManagerTest.kt
+++ tests/cts/net/src/android/net/cts/EthernetManagerTest.kt
@@ -46,10 +46,10 @@ import android.net.MacAddress
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkCapabilities.NET_CAPABILITY_NOT_CONGESTED
+import android.net.NetworkCapabilities.NET_CAPABILITY_NOT_METERED
 import android.net.NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED
 import android.net.NetworkCapabilities.NET_CAPABILITY_TEMPORARILY_NOT_METERED
 import android.net.NetworkCapabilities.NET_CAPABILITY_TRUSTED
-import android.net.NetworkCapabilities.TRANSPORT_ETHERNET
 import android.net.NetworkCapabilities.TRANSPORT_TEST
 import android.net.NetworkCapabilities.TRANSPORT_USB
 import android.net.NetworkRequest
@@ -127,7 +127,7 @@ private val DEFAULT_IP_CONFIGURATION = IpConfiguration(
 )
 private val ETH_REQUEST: NetworkRequest = NetworkRequest.Builder()
         .addTransportType(TRANSPORT_TEST)
-        .addTransportType(TRANSPORT_ETHERNET)
+        .addCapability(NET_CAPABILITY_NOT_METERED)
         .removeCapability(NET_CAPABILITY_TRUSTED)
         .build()
 private val LOCAL_REQUEST: NetworkRequest = NetworkRequest.Builder()
