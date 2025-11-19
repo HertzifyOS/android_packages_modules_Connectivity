f6a759ae3aafccf32a6a5ba19417edd5dd5395b5
Motomu Utsumi <motomuman@google.com>
2025-11-19 21:33:52 +0900

    bpf/progs/netd: Add map to enable local net access note op
    Introduce local_net_note_op_enabled_map to control writing
    local network access note op events to the
    local_net_note_op_ringbuf.
    
    Test: TH
    Bug: 462217324
    Flag: EXEMPT UNSUPPORTED_LANGUAGE
    Change-Id: Ia781eeff448e553da7704c431a5d21934f241379

--- bpf/progs/netd.h
+++ bpf/progs/netd.h
@@ -167,6 +167,7 @@ ASSERT_STRING_EQUAL(XT_BPF_DENYLIST_PROG_PATH,  BPF_NETD_PATH "prog_netd_skfilte
     BPF_NETD_PATH "map_netd_uid_migration_enabled_map"
 #define LOCAL_NET_NOTE_OP_RINGBUF_PATH BPF_NETD_PATH "map_netd_local_net_note_op_ringbuf"
 #define LOCAL_NET_NOTE_OP_CACHE_MAP_PATH BPF_NETD_PATH "map_netd_local_net_note_op_cache_map"
+#define LOCAL_NET_NOTE_OP_ENABLED_MAP_PATH BPF_NETD_PATH "map_netd_local_net_note_op_enabled_map"
 
 #define L4S_INGRESS_ETHER_PROG_PATH   BPF_NETD_PATH "prog_netd_schedcls_ingress_accecn_eth"
 #define L4S_EGRESS_ETHER_PROG_PATH    BPF_NETD_PATH "prog_netd_schedcls_egress_accecn_eth"
