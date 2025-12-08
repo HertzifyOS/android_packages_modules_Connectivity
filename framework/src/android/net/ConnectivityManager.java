75967243aaa24edfbd693546a58964081b8b9b86
Motomu Utsumi <motomuman@google.com>
2025-12-07 23:18:29 -0800

    Revert "Use string for CREATE_APP_SPECIFIC_NETWORK permission"
    Revert submission 35669132-CREATE_APP_SPECIFIC_NETWORK_API
    
    Reason for revert: CREATE_APP_SPECIFIC_NETWORK API should be released in 26Q2
    
    Reverted changes: /q/submissionid:35669132-CREATE_APP_SPECIFIC_NETWORK_API
    
    Bug: 435313135
    Change-Id: Ie61c560fb715df00733411f478f58586931b3add

--- framework/src/android/net/ConnectivityManager.java
+++ framework/src/android/net/ConnectivityManager.java
@@ -3967,10 +3967,6 @@ public class ConnectivityManager {
         }
     }
 
-    // TODO: Use android.Manifest.permission.CREATE_APP_SPECIFIC_NETWORK once it's available.
-    private static final String PERMISSION_CREATE_APP_SPECIFIC_NETWORK =
-            "android.permission.CREATE_APP_SPECIFIC_NETWORK";
-
     /**
      * @hide
      * Register a NetworkAgent with ConnectivityService.
@@ -3979,7 +3975,7 @@ public class ConnectivityManager {
     @RequiresPermission(anyOf = {
             NetworkStack.PERMISSION_MAINLINE_NETWORK_STACK,
             android.Manifest.permission.NETWORK_FACTORY,
-            PERMISSION_CREATE_APP_SPECIFIC_NETWORK})
+            android.Manifest.permission.CREATE_APP_SPECIFIC_NETWORK})
     public NetworkAndAgentRegistryParcelable registerNetworkAgent(
             @NonNull INetworkAgent na, @NonNull NetworkInfo ni,
             @NonNull LinkProperties lp, @NonNull NetworkCapabilities nc,
@@ -3996,7 +3992,7 @@ public class ConnectivityManager {
     @RequiresPermission(anyOf = {
             NetworkStack.PERMISSION_MAINLINE_NETWORK_STACK,
             android.Manifest.permission.NETWORK_FACTORY,
-            PERMISSION_CREATE_APP_SPECIFIC_NETWORK})
+            android.Manifest.permission.CREATE_APP_SPECIFIC_NETWORK})
     public NetworkAndAgentRegistryParcelable registerNetworkAgent(
             @NonNull INetworkAgent na, @NonNull NetworkInfo ni,
             @NonNull LinkProperties lp, @NonNull NetworkCapabilities nc,
