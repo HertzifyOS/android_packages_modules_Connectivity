b21f18742f5ffdb02dd4c0ab4d72aec96b25e302
Maciej Żenczykowski <maze@google.com>
2026-02-25 15:15:31 -0800

    dscpPolicy: move a chunk of code & unindent it
    (this doesn't work, purely to make next commit easier to read)
    
    Test: N/A, this DOES NOT build
    Flag: EXEMPT PURE_REFACTOR
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: Id27930583eb9054e8cbedc6ef0622f5c4fdba233

--- bpf/progs/dscpPolicy.c
+++ bpf/progs/dscpPolicy.c
@@ -38,6 +38,69 @@ static inline __always_inline uint64_t calculate_u64(uint64_t v) {
     return v;
 }
 
+    // Lookup failure cannot happen on an array with MAX_POLICIES entries.
+    // While 'continue' would make logical sense here, 'return' should be
+    // easier for the verifier to analyze.
+    if (!policy) return;
+
+    // Think of 'nomatch' as a 64-bit boolean: false iff zero, true iff non-zero.
+    // Start off with nomatch being false, ie. we assume things *are* matching.
+    uint64_t nomatch = 0;
+
+    // Due to 'a ^ b' being 0 iff a == b:
+    //   nomatch |= a ^ b
+    // should/can be read as:
+    //   nomatch ||= (a != b)
+    // which you can also think of as:
+    //   match &&= (a == b)
+
+    // If policy iface index does not match skb, then skip to next policy.
+    nomatch |= (policy->ifindex ^ skb->ifindex);
+
+    // policy->match_* are normal booleans, and should thus always be 0 or 1,
+    // thus you can think of these as:
+    //   if (policy->match_foo) match &&= (foo == policy->foo);
+    nomatch |= policy->match_proto * (protocol ^ policy->proto);
+    nomatch |= policy->match_src_ip * v6_not_equal(src_ip, policy->src_ip);
+    nomatch |= policy->match_dst_ip * v6_not_equal(dst_ip, policy->dst_ip);
+    nomatch |= policy->match_src_port * (sport ^ policy->src_port);
+
+    // Since these values are u16s (<=63 bits), we can rely on u64 subtraction
+    // underflow setting the topmost bit.  Basically, you can think of:
+    //   nomatch |= (a - b) >> 63
+    // as:
+    //   match &&= (a >= b)
+    uint64_t dport64 = dport;  // Note: dst_port_{start_end} range is inclusive of both ends.
+    nomatch |= calculate_u64(dport64 - policy->dst_port_start) >> 63;
+    nomatch |= calculate_u64(policy->dst_port_end - dport64) >> 63;
+
+    // score is 0x10000 for each matched field (proto, src_ip, dst_ip, src_port)
+    // plus 1..0x10000 for the dst_port range match (smaller for bigger ranges)
+    uint64_t score = 0;
+    score += policy->match_proto;  // reminder: match_* are boolean, thus 0 or 1
+    score += policy->match_src_ip;
+    score += policy->match_dst_ip;
+    score += policy->match_src_port;
+    score += 1;  // for a 1 element dst_port_{start,end} range
+    score <<= 16;  // scale up: ie. *= 0x10000
+    // now reduce score if the dst_port range is more than a single element
+    // we want to prioritize (ie. better score) matches of smaller ranges
+    score -= (policy->dst_port_end - policy->dst_port_start);  // -= 0..0xFFFF
+
+    // Here we need:
+    //   match &&= (score > best_score)
+    // which is the same as
+    //   match &&= (score >= best_score + 1)
+    // > not >= because we want equal score matches to prefer choosing earlier policies
+    nomatch |= calculate_u64(score - best_score - 1) >> 63;
+
+    COMPILER_FORCE_CALCULATION(nomatch);
+    if (nomatch) continue;
+
+    // only reachable if we matched the policy and (score > best_score)
+    best_score = score;
+    new_dscp = policy->dscp_val;
+
 static inline __always_inline void match_policy(struct __sk_buff* skb, const bool ipv4) {
     void* data = (void*)(long)skb->data;
     const void* data_end = (void*)(long)skb->data_end;
@@ -174,68 +237,6 @@ static inline __always_inline void match_policy(struct __sk_buff* skb, const boo
             policy = bpf_ipv6_dscp_policies_map_lookup_elem(&key);
         }
 
-        // Lookup failure cannot happen on an array with MAX_POLICIES entries.
-        // While 'continue' would make logical sense here, 'return' should be
-        // easier for the verifier to analyze.
-        if (!policy) return;
-
-        // Think of 'nomatch' as a 64-bit boolean: false iff zero, true iff non-zero.
-        // Start off with nomatch being false, ie. we assume things *are* matching.
-        uint64_t nomatch = 0;
-
-        // Due to 'a ^ b' being 0 iff a == b:
-        //   nomatch |= a ^ b
-        // should/can be read as:
-        //   nomatch ||= (a != b)
-        // which you can also think of as:
-        //   match &&= (a == b)
-
-        // If policy iface index does not match skb, then skip to next policy.
-        nomatch |= (policy->ifindex ^ skb->ifindex);
-
-        // policy->match_* are normal booleans, and should thus always be 0 or 1,
-        // thus you can think of these as:
-        //   if (policy->match_foo) match &&= (foo == policy->foo);
-        nomatch |= policy->match_proto * (protocol ^ policy->proto);
-        nomatch |= policy->match_src_ip * v6_not_equal(src_ip, policy->src_ip);
-        nomatch |= policy->match_dst_ip * v6_not_equal(dst_ip, policy->dst_ip);
-        nomatch |= policy->match_src_port * (sport ^ policy->src_port);
-
-        // Since these values are u16s (<=63 bits), we can rely on u64 subtraction
-        // underflow setting the topmost bit.  Basically, you can think of:
-        //   nomatch |= (a - b) >> 63
-        // as:
-        //   match &&= (a >= b)
-        uint64_t dport64 = dport;  // Note: dst_port_{start_end} range is inclusive of both ends.
-        nomatch |= calculate_u64(dport64 - policy->dst_port_start) >> 63;
-        nomatch |= calculate_u64(policy->dst_port_end - dport64) >> 63;
-
-        // score is 0x10000 for each matched field (proto, src_ip, dst_ip, src_port)
-        // plus 1..0x10000 for the dst_port range match (smaller for bigger ranges)
-        uint64_t score = 0;
-        score += policy->match_proto;  // reminder: match_* are boolean, thus 0 or 1
-        score += policy->match_src_ip;
-        score += policy->match_dst_ip;
-        score += policy->match_src_port;
-        score += 1;  // for a 1 element dst_port_{start,end} range
-        score <<= 16;  // scale up: ie. *= 0x10000
-        // now reduce score if the dst_port range is more than a single element
-        // we want to prioritize (ie. better score) matches of smaller ranges
-        score -= (policy->dst_port_end - policy->dst_port_start);  // -= 0..0xFFFF
-
-        // Here we need:
-        //   match &&= (score > best_score)
-        // which is the same as
-        //   match &&= (score >= best_score + 1)
-        // > not >= because we want equal score matches to prefer choosing earlier policies
-        nomatch |= calculate_u64(score - best_score - 1) >> 63;
-
-        COMPILER_FORCE_CALCULATION(nomatch);
-        if (nomatch) continue;
-
-        // only reachable if we matched the policy and (score > best_score)
-        best_score = score;
-        new_dscp = policy->dscp_val;
     }
 
     // Update cache with found policy.
